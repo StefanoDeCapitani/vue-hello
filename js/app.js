2acf21de8a2b31790abcd2dd424cc75fb5bc9862
Stefano De Capitani <stefano.deca@live.com>
2021-10-28 15:35:32 +0200

    Title and image displayed

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
-const app = new Vue({
-  el: "#root",
+Vue.config.devtools = true;
+
+window.addEventListener("DOMContentLoaded", () => {
+  const app = new Vue({
+    el: "#root",
+    data: {
+      title: "Hello World!",
+      urlImage: "../img/monitor.jpg",
+      altImage: "Postazione desktop con monitor",
+    },
+  });
 });
